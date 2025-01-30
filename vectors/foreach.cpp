e6bae0a995d2aa29235ba1f850f565287f960fd6
notarkhit <notarkhit@gmail.com>
2025-01-30 07:00:21 +0530

    initial commit

---
+++ vectors/foreach.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+
+int main() {
+
+	std::vector<int> vectorArray;
+	
+	std::vector<int> vectorArray2;
+
+	vectorArray = {1,2,3,4,5,6,7};
+	vectorArray2 = {1,2,3,4,5,6,7};
+
+	for(int element : vectorArray) {
+		vectorArray2.push_back(element);
+	}
+
+	for (int element : vectorArray2){
+		std::cout << element ;
+	}
+
+	return 0;
+}
